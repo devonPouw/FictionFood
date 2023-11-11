270c35b40eae0187fa357469485fcd0d2fc15799
Devon Pouw <devon_pouw@hotmail.com>
2023-11-11 12:36:41 +0100

    Initial commit

---
+++ src/main/java/com/portfolio/lembas/LembasApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.lembas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembasApplication.class, args);
+	}
+
+}
