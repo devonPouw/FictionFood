7b1f65ce46891606a5eed6e032a577a0e12ca6ac
Devon Pouw <devon_pouw@hotmail.com>
2023-11-11 15:13:47 +0100

    UserServiceImplementation created

---
+++ src/main/java/com/portfolio/lembas/user/UserServiceImplementation.java
@@ -0,0 +1,17 @@
+package com.portfolio.lembas.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserServiceImplementation implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public Optional<User> getByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
