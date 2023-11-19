0302337af89095a6a1fcf7243a565de4c053e3a8
Devon Pouw <devon_pouw@hotmail.com>
2023-11-19 17:24:50 +0100

    some formatting done

--- fictionfood/src/main/java/com/portfolio/fictionfood/recipeingredient/RecipeIngredient.java
+++ fictionfood/src/main/java/com/portfolio/fictionfood/recipeingredient/RecipeIngredient.java
@@ -4,14 +4,17 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.portfolio.fictionfood.ingredient.Ingredient;
 import com.portfolio.fictionfood.recipe.Recipe;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import lombok.extern.jackson.Jacksonized;
 
+@Jacksonized
 @Entity
-@NoArgsConstructor
 @Getter
 @Setter
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class RecipeIngredient {
 
     @Id
@@ -31,11 +34,4 @@ public class RecipeIngredient {
 
     @Enumerated(EnumType.STRING)
     private Unit unit;
-
-    public RecipeIngredient(double quantity, Ingredient ingredient, Recipe recipe, Unit unit) {
-        this.quantity = quantity;
-        this.ingredient = ingredient;
-        this.recipe = recipe;
-        this.unit = unit;
-    }
 }
