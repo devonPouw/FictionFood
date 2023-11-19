fddbd72ba6ec9b8415548008029bf9ece764aaa5
Devon Pouw <devon_pouw@hotmail.com>
2023-11-19 16:47:40 +0100

    added unit enum

---
+++ fictionfood/src/main/java/com/portfolio/fictionfood/recipeingredient/Unit.java
@@ -0,0 +1,12 @@
+package com.portfolio.fictionfood.recipeingredient;
+
+public enum Unit {
+    KILOGRAM,
+    GRAM,
+    LITRE,
+    CENTILITRE,
+    MILLILITRE,
+    TABLESPOON,
+    TEASPOON,
+    PIECE
+}
