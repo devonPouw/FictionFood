d7d9a5ec9f6217e378e4db4882846f8553101935
Devon Pouw <devon_pouw@hotmail.com>
2023-12-04 23:31:25 +0100

    root modified with router and http common added

--- vite-frontend/src/Root.tsx
+++ vite-frontend/src/Root.tsx
@@ -4,33 +4,49 @@ import Header from "./components/header/Header"
 import {
   createBrowserRouter,
   Link,
+  Navigate,
+  Outlet,
   Route,
   RouterProvider,
   Routes,
 } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { useContext } from "react";
+import { AuthContext } from "./misc/AuthContext";
+import AddRecipe from "./components/recipe/AddRecipe";
+
+type Props = {}
+
+const PrivateRroutes = () => {
+  const {authenticated} = useContext(AuthContext)
+
+  if(!authenticated) return <Navigate to='/home' replace />
+
+  return <Outlet />
+}
 
 const router = createBrowserRouter([
   { path: "*", Component: Root },
 ]);
 
-export default function App() {
+ const App = (props: Props) => {
   return <RouterProvider router={router} />;
 }
 function Root() {
-  
  
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
        <Header />
     <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route element={<PrivateRroutes />} />
     <Route path="/" element={<Home />} />
-    {/* <Route path="/blog/*" element={<BlogApp />} />
-    <Route path="/users/*" element={<UserApp />} /> */}
+    <Route path="recipes/new" element={<AddRecipe />} />
   </Routes>
   </ThemeProvider>
 );
 }
     
-     
+export default App
     
