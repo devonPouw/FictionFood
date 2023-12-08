57c18b0e1552f509a89494bd3c628bac3f843a25
Devon Pouw <devon_pouw@hotmail.com>
2023-12-08 20:57:10 +0100

    Login page created

---
+++ vite-frontend/src/components/auth/Login.tsx
@@ -0,0 +1,130 @@
+import {  Link, NavigateFunction, useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+    
+import { Button } from "@/components/ui/button"
+import {
+      Form,
+      FormControl,
+      FormField,
+      FormItem,
+      FormLabel,
+      FormMessage,
+    } from "@/components/ui/form"
+     import { Input } from "@/components/ui/input"
+import { login } from "@/services/auth/auth.service"; 
+import { useState } from "react";
+
+     type Props = {}
+
+const Login: React.FC<Props> = () => {
+  const navigate: NavigateFunction = useNavigate();
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
+  
+  const handleLogin = (formValue: { username: string; password: string }) => {
+    const { username, password } = formValue;
+
+    setMessage("");
+    setLoading(true);
+
+    login(username, password).then(
+      () => {
+        navigate("/");
+        window.location.reload();
+      },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setLoading(false);
+        setMessage(resMessage);
+      }
+    );
+  };
+
+  const formSchema = z.object({
+    username: z.string().max(24, {
+      message: "Not more than 24 characters allowed.",
+    }),
+    password: z.string().max(24, {
+      message: "Not more than 24 characters allowed.",
+    }),
+  }).required();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: ""
+    },
+  })
+
+  return (
+    <div className="container place-self-center">
+      <div className="container">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleLogin)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" {...field} />
+                  </FormControl>
+                  <FormMessage
+                  autoCorrect="false" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="********" {...field} />
+                  </FormControl>
+                  <FormMessage
+                  autoCorrect="false"
+                   />
+                </FormItem>
+              )}
+            />
+            <div className="w-full flex justify-center">
+              <div>
+            <div>
+            <Button className="w-32 md:24 border-2" type="submit">Submit</Button>
+              {loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                </div>
+                {message && (
+              <div className="form-group text-center text-red-700">
+                  {message}
+                </div>
+             
+            )}
+            </div>
+            </div>
+          </form>
+        </Form>
+        <div className='container pt-4 flex h-14 items-center justify-center font-bold'>
+          <Link to={"/register"}>
+            <span className="hover:text-slate-400">Don't have an account yet? Click here to register!</span>
+          </Link>
+        </div>
+        </div>
+        </div>
+  );
+}
+export default Login
