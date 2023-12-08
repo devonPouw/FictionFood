33a76e7ad08573850931963908aa37358f5fa1b2
Devon Pouw <devon_pouw@hotmail.com>
2023-12-08 20:57:17 +0100

    register page created

---
+++ vite-frontend/src/components/auth/Register.tsx
@@ -0,0 +1,153 @@
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { useState } from "react";
+
+import IUser from "@/types/User";
+import { register } from "@/services/auth/auth.service";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+type Props = {}
+
+const Register: React.FC<Props> = () => {
+    const navigate: NavigateFunction = useNavigate();
+
+  const [successful, setSuccessful] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
+
+  const formSchema = z.object({
+    nickname: z.string().min(3, {
+        message: "Must be 3 or more characters long"
+    }).max(15, {
+        message: "Must be 15 or fewer characters long",
+      }),
+    username: z.string().max(15, {
+      message: "Must be 15 or fewer characters long",
+    }),
+    email: z.string().email({
+         message: "Invalid email address" 
+        }),
+    password: z.string().min(8, {
+        message: "Must be 8 or more characters long"
+    })
+    .max(24, {
+      message: "Not more than 24 characters allowed.",
+    }),
+  }).required();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+        nickname: "",
+        username: "",
+        email: "",
+        password: "",
+    },
+  })
+
+const handleRegister = (formValue: IUser) => {
+    const { nickname, username, email,  password } = formValue;
+
+    register(nickname, username, email, password).then(
+      (response) => {
+        setMessage(response.data.message);
+        setSuccessful(true);
+        navigate("/login");
+        window.location.reload();
+      },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setMessage(resMessage);
+        setSuccessful(false);
+      }
+    );
+  };
+  return( 
+    <div>
+        <Form {...form}>
+        {!successful && (
+          <form onSubmit={form.handleSubmit(handleRegister)} className="space-y-8">
+          <FormField
+              control={form.control}
+              name="nickname"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nickname</FormLabel>
+                  <FormControl>
+                    <Input placeholder="nickname" {...field} />
+                  </FormControl>
+                  <FormMessage
+                  autoCorrect="false" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="given@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" {...field} />
+                  </FormControl>
+                  <FormMessage
+                  autoCorrect="false" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage
+                  autoCorrect="false"
+                   />
+                </FormItem>
+              )}
+            />
+            <div>
+            <Button type="submit">Submit</Button>
+                </div>
+                {message && (
+              <div className="form-group">
+                <div  className={
+                    successful ? "alert alert-success" : "alert alert-danger"} role="alert">
+                  {message}
+                </div>
+              </div>
+            )}
+          </form>
+        )}
+        </Form>
+        </div>
+  )
+}
+
+export default Register
