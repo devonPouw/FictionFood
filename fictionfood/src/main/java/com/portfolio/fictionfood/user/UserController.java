ef8801cadf6dc8a887cb465bf4175e5ca169f8a9
Devon Pouw <devon_pouw@hotmail.com>
2023-12-23 16:49:13 +0100

    added a ChangePassword patchmapping

--- fictionfood/src/main/java/com/portfolio/fictionfood/user/UserController.java
+++ fictionfood/src/main/java/com/portfolio/fictionfood/user/UserController.java
@@ -1,31 +1,27 @@
 package com.portfolio.fictionfood.user;
 
-import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.NoSuchElementException;
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final UserService service;
 
-    @JsonView(UserViews.viewMe.class)
-    @GetMapping("/me")
-    public ResponseEntity<User> getCurrentUser(@AuthenticationPrincipal User currentUser) {
-        try {
-            return ResponseEntity.ok(currentUser);
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.notFound().build();
-        }
+    @PatchMapping
+    public ResponseEntity<?> changePassword(
+            @RequestBody ChangePasswordRequest request,
+            Principal connectedUser
+    ) {
+        service.changePassword(request, connectedUser);
+        return ResponseEntity.ok().build();
     }
 }
