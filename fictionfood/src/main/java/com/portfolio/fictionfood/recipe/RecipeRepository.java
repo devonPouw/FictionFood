179165ccae8604814ae0aaa4cab3dcab77be8f95
Devon Pouw <devon_pouw@hotmail.com>
2024-01-11 15:38:45 +0100

    repository methods added to recipe

--- fictionfood/src/main/java/com/portfolio/fictionfood/recipe/RecipeRepository.java
+++ fictionfood/src/main/java/com/portfolio/fictionfood/recipe/RecipeRepository.java
@@ -2,10 +2,17 @@ package com.portfolio.fictionfood.recipe;
 
 import com.portfolio.fictionfood.user.User;
 import io.micrometer.common.lang.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    Page<Recipe> findByIsPublished(@NonNull Boolean isPublished, Pageable pageable);
+
+    Optional<Recipe> findByIdAndIsPublished(@NonNull Long id, @NonNull Boolean isPublished);
+
     Long countByAuthorAndDatePublishedAfter(@NonNull User author, @NonNull LocalDateTime oneMonthAgo);
 }
