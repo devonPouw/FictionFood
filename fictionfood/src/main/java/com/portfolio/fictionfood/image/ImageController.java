70f13c6f9c04a006f0ce5cdbd4ca5b076fc8e89f
Devon Pouw <devon_pouw@hotmail.com>
2024-01-17 16:02:46 +0100

    getmapping by id to check if the database shows the image

--- fictionfood/src/main/java/com/portfolio/fictionfood/image/ImageController.java
+++ fictionfood/src/main/java/com/portfolio/fictionfood/image/ImageController.java
@@ -1,8 +1,16 @@
 package com.portfolio.fictionfood.image;
 
+import com.portfolio.fictionfood.recipe.RecipeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 @RestController
 @RequestMapping("/api/images")
@@ -10,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImageController {
 
     private final ImageService imageService;
+    private final RecipeRepository recipeRepository;
 
 //    @PostMapping
 //    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
@@ -17,11 +26,11 @@ public class ImageController {
 //        return ResponseEntity.status(HttpStatus.OK).body(uploadImage);
 //    }
 
-//    @GetMapping("/{fileName}")
-//    public ResponseEntity<?> downloadImage(@PathVariable String fileName) {
-//        byte[] imageData = imageService.downloadImage(fileName);
-//        return ResponseEntity.status(HttpStatus.OK)
-//                .contentType(MediaType.valueOf(IMAGE_PNG_VALUE))
-//                .body(imageData);
-//    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> downloadImage(@PathVariable long id) {
+        byte[] imageData = imageService.downloadImage(recipeRepository.findById(id).orElseThrow().getImage().getName());
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf(IMAGE_PNG_VALUE))
+                .body(imageData);
+    }
 }
