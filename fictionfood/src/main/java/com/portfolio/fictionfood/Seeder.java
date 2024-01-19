8748994e398d04f670b2f41d1b13df6b79f32e65
Devon Pouw <devon_pouw@hotmail.com>
2024-01-19 13:21:56 +0100

    seeder filled with more recipes

--- fictionfood/src/main/java/com/portfolio/fictionfood/Seeder.java
+++ fictionfood/src/main/java/com/portfolio/fictionfood/Seeder.java
@@ -45,10 +45,13 @@ public class Seeder implements CommandLineRunner {
     }
 
     private void seedRecipes() throws IOException {
-        Recipe recipe = new Recipe();
-        Recipe recipe1 = new Recipe();
-        Recipe recipe2 = new Recipe();
-        recipeRepository.saveAll(Set.of(recipe, recipe1, recipe2));
+        var recipe = new Recipe();
+        var recipe1 = new Recipe();
+        var recipe2 = new Recipe();
+        var recipe3 = new Recipe();
+        var recipe4 = new Recipe();
+
+        recipeRepository.saveAll(Set.of(recipe, recipe1, recipe2, recipe3, recipe4));
 
         var user = User.builder()
                 .nickname("GordonRamsay")
@@ -68,8 +71,17 @@ public class Seeder implements CommandLineRunner {
         category2.setName("Lord of the Rings");
         Category category3 = new Category();
         category3.setName("Star Wars");
+        Category category4 = new Category();
+        category4.setName("Italian");
+        Category category5 = new Category();
+        category5.setName("Breakfast");
+        Category category6 = new Category();
+        category6.setName("Grilled");
+        Category category7 = new Category();
+        category7.setName("Comfort Food");
 
-        categoryRepository.saveAll(Set.of(category, category1, category2, category3));
+        categoryRepository.saveAll(Set.of(category, category1, category2, category3, category4, category5, category6,
+                category7));
 
         Ingredient ingredient = new Ingredient();
         ingredient.setName("Flour");
@@ -83,8 +95,19 @@ public class Seeder implements CommandLineRunner {
         ingredient4.setName("Steak");
         Ingredient ingredient5 = new Ingredient();
         ingredient5.setName("Gravy");
+        Ingredient ingredient6 = new Ingredient();
+        ingredient6.setName("Tomato Sauce");
+        Ingredient ingredient7 = new Ingredient();
+        ingredient7.setName("Parmesan Cheese");
+        Ingredient ingredient8 = new Ingredient();
+        ingredient8.setName("Spaghetti");
+        Ingredient ingredient9 = new Ingredient();
+        ingredient9.setName("Olive Oil");
+        Ingredient ingredient10 = new Ingredient();
+        ingredient10.setName("Mozzarella");
 
-        ingredientRepository.saveAll(Set.of(ingredient, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5));
+        ingredientRepository.saveAll(Set.of(ingredient, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5,
+                ingredient6, ingredient7, ingredient8, ingredient9, ingredient10));
 
         var recipeIngredient = RecipeIngredient.builder()
                 .ingredient(ingredient)
@@ -128,19 +151,59 @@ public class Seeder implements CommandLineRunner {
                 .recipe(recipe2)
                 .build();
 
+        var recipeIngredient6 = RecipeIngredient.builder()
+                .ingredient(ingredient6)
+                .quantity(400D)
+                .unit(Unit.GRAM)
+                .recipe(recipe3)
+                .build();
+
+        var recipeIngredient7 = RecipeIngredient.builder()
+                .ingredient(ingredient7)
+                .quantity(50D)
+                .unit(Unit.GRAM)
+                .recipe(recipe3)
+                .build();
+
+        var recipeIngredient8 = RecipeIngredient.builder()
+                .ingredient(ingredient8)
+                .quantity(300D)
+                .unit(Unit.GRAM)
+                .recipe(recipe3)
+                .build();
+
+        var recipeIngredient9 = RecipeIngredient.builder()
+                .ingredient(ingredient9)
+                .quantity(2D)
+                .unit(Unit.TABLESPOON)
+                .recipe(recipe3)
+                .build();
+
+        var recipeIngredient10 = RecipeIngredient.builder()
+                .ingredient(ingredient10)
+                .quantity(150D)
+                .unit(Unit.GRAM)
+                .recipe(recipe3)
+                .build();
+
         recipeIngredientRepository.saveAll(Set.of(recipeIngredient, recipeIngredient1, recipeIngredient2,
-                recipeIngredient3, recipeIngredient4, recipeIngredient5));
+                recipeIngredient3, recipeIngredient4, recipeIngredient5, recipeIngredient6, recipeIngredient7,
+                recipeIngredient8, recipeIngredient9, recipeIngredient10));
 
         MultipartFile[] image;
         image = new MultipartFile[]{
-                imageService.seedImage("src/Variety-fruits-vegetables.jpg"),
+                imageService.seedImage("src/pasta.jpg"),
                 imageService.seedImage("src/bacon&eggs.jpg"),
                 imageService.seedImage("src/steak.jpg"),
+                imageService.seedImage("src/spaghetti-bolognese.jpg"),
+                imageService.seedImage("src/pizza-margarita.jpg"),
         };
 
         imageService.uploadRecipeImage(image[0], recipe);
         imageService.uploadRecipeImage(image[1], recipe1);
         imageService.uploadRecipeImage(image[2], recipe2);
+        imageService.uploadRecipeImage(image[3], recipe3);
+        imageService.uploadRecipeImage(image[4], recipe4);
 
         recipe.setTitle("Pasta");
         recipe.setSummary("This pasta is really good");
@@ -175,6 +238,29 @@ public class Seeder implements CommandLineRunner {
         recipe2.setRecipeIngredients(Set.of(recipeIngredient, recipeIngredient1));
         recipe2.setImage(imageRepository.findByRecipe(recipe2).orElseThrow());
 
-        recipeRepository.saveAll(Set.of(recipe, recipe1, recipe2));
+        recipe3.setTitle("Spaghetti Bolognese");
+        recipe3.setSummary("Classic Italian dish");
+        recipe3.setContent("A delicious pasta dish with rich tomato sauce and Parmesan cheese.");
+        recipe3.setIsPublished(true);
+        recipe3.setAuthor(user);
+        recipe3.setRating(BigDecimal.valueOf(4.8));
+        recipe3.setDatePublished(LocalDateTime.now().minusSeconds(300));
+        recipe3.setCategories(Set.of(category, category4, category5));
+        recipe3.setRecipeIngredients(Set.of(recipeIngredient6, recipeIngredient7, recipeIngredient8, recipeIngredient9, recipeIngredient10));
+        recipe3.setImage(imageRepository.findByRecipe(recipe3).orElseThrow());
+
+        recipe4.setTitle("Margherita Pizza");
+        recipe4.setSummary("Classic Italian pizza");
+        recipe4.setContent("A simple and delicious pizza with tomato, mozzarella, and basil.");
+        recipe4.setIsPublished(true);
+        recipe4.setAuthor(user);
+        recipe4.setRating(BigDecimal.valueOf(4.9));
+        recipe4.setDatePublished(LocalDateTime.now().minusSeconds(180));
+        recipe4.setCategories(Set.of(category, category4));
+        recipe4.setRecipeIngredients(Set.of(recipeIngredient, recipeIngredient6, recipeIngredient7, recipeIngredient9, recipeIngredient10));
+        recipe4.setImage(imageRepository.findByRecipe(recipe4).orElseThrow());
+
+
+        recipeRepository.saveAll(Set.of(recipe, recipe1, recipe2, recipe3, recipe4));
     }
 }
