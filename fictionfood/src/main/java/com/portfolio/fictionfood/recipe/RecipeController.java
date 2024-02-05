8310aed1605f71bba38c833a0b52ea7fe1d43e9b
Devon Pouw <devon_pouw@hotmail.com>
2024-02-05 17:35:04 +0100

    recipe controller split to service and controller

--- fictionfood/src/main/java/com/portfolio/fictionfood/recipe/RecipeController.java
+++ fictionfood/src/main/java/com/portfolio/fictionfood/recipe/RecipeController.java
@@ -1,25 +1,12 @@
 package com.portfolio.fictionfood.recipe;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.portfolio.fictionfood.authentication.token.TokenRepository;
-import com.portfolio.fictionfood.category.Category;
-import com.portfolio.fictionfood.category.CategoryRepository;
-import com.portfolio.fictionfood.image.ImageRepository;
-import com.portfolio.fictionfood.image.ImageService;
-import com.portfolio.fictionfood.image.MultipartImage;
-import com.portfolio.fictionfood.image.RecipeImage;
-import com.portfolio.fictionfood.ingredient.Ingredient;
-import com.portfolio.fictionfood.ingredient.IngredientRepository;
-import com.portfolio.fictionfood.recipeingredient.RecipeIngredient;
-import com.portfolio.fictionfood.recipeingredient.RecipeIngredientDto;
+import com.portfolio.fictionfood.exception.UnauthorizedException;
 import com.portfolio.fictionfood.user.User;
 import com.portfolio.fictionfood.user.UserRole;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -27,152 +14,73 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/recipes")
 public class RecipeController {
 
-    private final IngredientRepository ingredientRepository;
-    private final ImageService imageService;
-    private final ImageRepository imageRepository;
-    private final TokenRepository tokenRepository;
-    private final ObjectMapper objectMapper;
+    private static final Logger logger = LoggerFactory.getLogger(RecipeController.class);
+    private final RecipeService recipeService;
+    private final RecipeRepository recipeRepository;
     @Value("${app.recipe-rules.max-per-month}")
     int maxRecipesPerMonth;
-    @Autowired
-    private RecipeRepository recipeRepository; //Without @Autowired, Function below will not work.
-    Function<User, Boolean> hasNotExceededMaxPerMonth = user ->
-            recipeRepository.countByAuthorAndDatePublishedAfter(user, LocalDateTime.now().minusMonths(1)) < maxRecipesPerMonth;
-    @Autowired
-    private CategoryRepository categoryRepository;
 
     @GetMapping("/{id}")
-    public ResponseEntity<RecipeInfoDto> getRecipeById(@PathVariable("id") long id,
-                                                       @AuthenticationPrincipal User currentUser) {
+    public ResponseEntity<?> getRecipeById(@PathVariable("id") Long id, @AuthenticationPrincipal User currentUser) {
+        logger.info("Request received to fetch recipe with id: {}", id);
+
         try {
-            var recipe = recipeRepository.findById(id).orElseThrow();
-            if (!recipe.getAuthor().equals(currentUser) && !recipe.getIsPublished()) {
-                return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+            RecipeInfoDto recipeInfo = recipeService.getRecipeByIdAndUser(id, currentUser);
+            if (recipeInfo == null) {
+                logger.warn("No recipe found with id: {}", id);
+                return ResponseEntity.notFound().build();
             }
-            byte[] recipeImageData = imageService.downloadImage(recipe.getImage().getName());
-
-            var recipeInfo = RecipeInfoDto.builder()
-                    .title(recipe.getTitle())
-                    .summary(recipe.getSummary())
-                    .content(recipe.getContent())
-                    .recipeIngredients(recipe.getRecipeIngredients().stream().map(recipeIngredient -> new RecipeIngredientDto(
-                            recipeIngredient.getUnit(),
-                            recipeIngredient.getQuantity(),
-                            recipeIngredient.getIngredient().getName())).collect(Collectors.toSet()))
-                    .categories(recipe.getCategories().stream().map(Category::getName).collect(Collectors.toSet()))
-                    .author(recipe.getAuthor().getNickname())
-                    .recipeImage(recipeImageData)
-                    .build();
-
-            return new ResponseEntity<>(recipeInfo, HttpStatus.OK);
+            logger.debug("Returning recipe details for id: {}", id);
+            return ResponseEntity.ok(recipeInfo);
+        } catch (UnauthorizedException e) {
+            logger.error("Error fetching recipe with id: " + id, e);
+            return ResponseEntity.badRequest().build();
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            logger.error("Error fetching recipe with id: " + id, e);
+            return ResponseEntity.internalServerError().build();
         }
     }
 
     @GetMapping
     public ResponseEntity<Map<String, Object>> getAllRecipes(@RequestParam(defaultValue = "0") int page,
                                                              @RequestParam(defaultValue = "10") int size) {
+        logger.info("Fetching all published recipes, page: {}, size: {}", page, size);
+
         try {
-            PageRequest paging = PageRequest.of(page, size, Sort.by("datePublished").descending());
-            Page<Recipe> pageRecipes = recipeRepository.findByIsPublished(true, paging);
-
-            List<Recipe> recipes = pageRecipes.getContent();
-            List<RecipeListDto> recipeDtos = new ArrayList<>();
-
-            for (Recipe recipe : recipes) {
-                byte[] imageData = imageService.downloadImage(recipe.getImage().getName());
-
-                recipeDtos.add(RecipeListDto.builder()
-                        .id(recipe.getId())
-                        .title(recipe.getTitle())
-                        .summary(recipe.getSummary())
-                        .categories(recipe.getCategories().stream().map(Category::getName).collect(Collectors.toSet()))
-                        .author(recipe.getAuthor().getNickname())
-                        .datePublished(recipe.getDatePublished())
-                        .rating(recipe.getRating())
-                        .imageData(imageData)
-                        .build());
-            }
-
-            Map<String, Object> responseBody = new HashMap<>();
-            responseBody.put("recipes", recipeDtos);
-            responseBody.put("currentPage", pageRecipes.getNumber());
-            responseBody.put("totalItems", pageRecipes.getTotalElements());
-            responseBody.put("totalPages", pageRecipes.getTotalPages());
-
-            return new ResponseEntity<>(responseBody, HttpStatus.OK);
+            Map<String, Object> response = recipeService.getAllPublishedRecipes(page, size);
+            logger.debug("Fetched {} recipes for page: {}", response.get("totalItems"), page);
+            return ResponseEntity.ok(response);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            logger.error("Error fetching recipes", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping
     public ResponseEntity<?> postRecipe(@RequestPart("recipe") String recipeJson,
-                                             @RequestPart("image") MultipartFile image,
-                                             @AuthenticationPrincipal User currentUser) {
+                                        @RequestPart("image") MultipartFile image,
+                                        @AuthenticationPrincipal User currentUser) {
+        logger.info("Attempting to post a new recipe by user: {}", currentUser.getUsername());
+
         if (!checkIfAllowedToPost(currentUser)) {
+            logger.warn("User: {} is not allowed to post more recipes", currentUser.getUsername());
             return new ResponseEntity<>(HttpStatus.TOO_MANY_REQUESTS);
         }
 
         try {
-            PostRecipeDto recipeDto = objectMapper.readValue(recipeJson, PostRecipeDto.class);
-
-            var recipe = new Recipe();
-            recipe.setTitle(recipeDto.getTitle());
-            recipe.setSummary(recipeDto.getSummary());
-            recipe.setContent(recipeDto.getContent());
-            recipe.setIsPublished(recipeDto.getIsPublished());
-            recipe.setAuthor(currentUser);
-            recipe.setRating(BigDecimal.valueOf(0.0));
-            recipe.setDatePublished(LocalDateTime.now());
-//                  recipe.setAmountOfReviews(0);
-            recipe.setCategories(new HashSet<>(Arrays.stream(recipeDto.getCategories())
-                    .map(categoryDto -> {
-                        Category category = new Category();
-                        category.setName(categoryDto);
-                        categoryRepository.save(category);
-                        return category;
-                    })
-                    .collect(Collectors.toSet())));
-            recipe.setRecipeIngredients(recipeDto.getRecipeIngredients()
-                    .stream()
-                    .map(recipeIngredientDto -> {
-                        RecipeIngredient recipeIngredient = new RecipeIngredient();
-                        recipeIngredient.setRecipe(recipe);
-
-                        Ingredient ingredient = ingredientRepository.findByNameIgnoringCase(recipeIngredientDto.getIngredient())
-                                .orElseGet(() -> {
-                                    Ingredient newIngredient = new Ingredient();
-                                    newIngredient.setName(recipeIngredientDto.getIngredient());
-                                    ingredientRepository.save(newIngredient);
-                                    return newIngredient;
-                                });
-
-                        recipeIngredient.setIngredient(ingredient);
-                        recipeIngredient.setQuantity(recipeIngredientDto.getQuantity());
-                        recipeIngredient.setUnit(recipeIngredientDto.getUnit());
-
-                        return recipeIngredient;
-                    })
-                    .collect(Collectors.toCollection(HashSet::new)));
-            recipeRepository.save(recipe);
-            imageService.uploadImage(image, recipe);
-            recipe.setImage(imageRepository.findByRecipe(recipe).orElseThrow());
-            return new ResponseEntity<>(recipeRepository.save(recipe), HttpStatus.CREATED);
+            Recipe recipe = recipeService.postRecipe(recipeJson, image, currentUser);
+            logger.info("Recipe successfully posted with ID: {}", recipe.getId());
+            return ResponseEntity.status(HttpStatus.CREATED).body(recipe);
         } catch (IOException e) {
+            logger.error("Error posting recipe", e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -182,8 +90,14 @@ public class RecipeController {
         recipeRepository.deleteById(id);
     }
 
-    boolean checkIfAllowedToPost(User user) {
-        return user.getRole().equals(UserRole.MODERATOR) || (user.getRole().equals(UserRole.CHEF)
-                && hasNotExceededMaxPerMonth.apply(user));
+    public boolean checkIfAllowedToPost(User user) {
+        if (user.getRole().equals(UserRole.MODERATOR)) {
+            return true;
+        }
+        if (user.getRole().equals(UserRole.CHEF)) {
+            long count = recipeRepository.countByAuthorAndDatePublishedAfter(user, LocalDateTime.now().minusMonths(1));
+            return count < maxRecipesPerMonth;
+        }
+        return false;
     }
 }
---
+++ fictionfood/src/main/java/com/portfolio/fictionfood/recipe/RecipeService.java
@@ -0,0 +1,135 @@
+package com.portfolio.fictionfood.recipe;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.portfolio.fictionfood.category.Category;
+import com.portfolio.fictionfood.category.CategoryRepository;
+import com.portfolio.fictionfood.exception.UnauthorizedException;
+import com.portfolio.fictionfood.image.ImageRepository;
+import com.portfolio.fictionfood.image.ImageService;
+import com.portfolio.fictionfood.ingredient.Ingredient;
+import com.portfolio.fictionfood.ingredient.IngredientRepository;
+import com.portfolio.fictionfood.recipeingredient.RecipeIngredient;
+import com.portfolio.fictionfood.recipeingredient.RecipeIngredientDto;
+import com.portfolio.fictionfood.user.User;
+import com.portfolio.fictionfood.user.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class RecipeService {
+    private final IngredientRepository ingredientRepository;
+    private final ImageService imageService;
+    private final ImageRepository imageRepository;
+    private final CategoryRepository categoryRepository;
+    private final RecipeRepository recipeRepository;
+    private final ObjectMapper objectMapper;
+
+    public RecipeInfoDto getRecipeByIdAndUser(long id, User currentUser) throws UnauthorizedException {
+        var recipe = recipeRepository.findById(id).orElseThrow();
+        if (currentUser == null || currentUser.getRole().equals(UserRole.CHEF))
+            if (!recipe.getAuthor().equals(currentUser) && !recipe.getIsPublished()) {
+                throw new UnauthorizedException("User is not authorized to access this recipe");
+            }
+
+        byte[] recipeImageData = imageService.downloadImage(recipe.getImage().getName());
+
+        return RecipeInfoDto.builder()
+                .title(recipe.getTitle())
+                .summary(recipe.getSummary())
+                .content(recipe.getContent())
+                .recipeIngredients(recipe.getRecipeIngredients().stream().map(recipeIngredient -> new RecipeIngredientDto(
+                        recipeIngredient.getUnit(),
+                        recipeIngredient.getQuantity(),
+                        recipeIngredient.getIngredient().getName())).collect(Collectors.toSet()))
+                .categories(recipe.getCategories().stream().map(Category::getName).collect(Collectors.toSet()))
+                .author(recipe.getAuthor().getNickname())
+                .recipeImage(recipeImageData)
+                .build();
+    }
+
+    public Map<String, Object> getAllPublishedRecipes(int page, int size) {
+        PageRequest paging = PageRequest.of(page, size, Sort.by("datePublished").descending());
+        Page<Recipe> pageRecipes = recipeRepository.findByIsPublished(true, paging);
+
+        List<RecipeListDto> recipeDtoList = pageRecipes.getContent().stream().map(recipe -> {
+            byte[] imageData = imageService.downloadImage(recipe.getImage().getName());
+            return RecipeListDto.builder()
+                    .id(recipe.getId())
+                    .title(recipe.getTitle())
+                    .summary(recipe.getSummary())
+                    .categories(recipe.getCategories().stream().map(Category::getName).collect(Collectors.toSet()))
+                    .author(recipe.getAuthor().getNickname())
+                    .datePublished(recipe.getDatePublished())
+                    .rating(recipe.getRating())
+                    .imageData(imageData)
+                    .build();
+        }).collect(Collectors.toList());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("recipes", recipeDtoList);
+        response.put("currentPage", pageRecipes.getNumber());
+        response.put("totalItems", pageRecipes.getTotalElements());
+        response.put("totalPages", pageRecipes.getTotalPages());
+
+        return response;
+    }
+
+    public Recipe postRecipe(String recipeJson, MultipartFile image, User currentUser) throws IOException {
+        PostRecipeDto recipeDto = objectMapper.readValue(recipeJson, PostRecipeDto.class);
+
+        var recipe = new Recipe();
+        recipe.setTitle(recipeDto.getTitle());
+        recipe.setSummary(recipeDto.getSummary());
+        recipe.setContent(recipeDto.getContent());
+        recipe.setIsPublished(recipeDto.getIsPublished());
+        recipe.setAuthor(currentUser);
+        recipe.setRating(BigDecimal.valueOf(0.0));
+        recipe.setDatePublished(LocalDateTime.now());
+//                  recipe.setAmountOfReviews(0);
+        recipe.setCategories(new HashSet<>(Arrays.stream(recipeDto.getCategories())
+                .map(categoryDto -> categoryRepository.findByNameIgnoringCase(categoryDto)
+                        .orElseGet(() -> {
+                            Category newCategory = new Category();
+                            newCategory.setName(categoryDto);
+                            return categoryRepository.save(newCategory);
+                        }))
+                .collect(Collectors.toSet())));
+        recipe.setRecipeIngredients(new HashSet<>(Arrays.stream(recipeDto.getRecipeIngredients())
+                .map(recipeIngredientDto -> {
+                    RecipeIngredient recipeIngredient = new RecipeIngredient();
+                    recipeIngredient.setRecipe(recipe);
+
+                    Ingredient ingredient = ingredientRepository.findByNameIgnoringCase(recipeIngredientDto.getIngredient())
+                            .orElseGet(() -> {
+                                Ingredient newIngredient = new Ingredient();
+                                newIngredient.setName(recipeIngredientDto.getIngredient());
+                                ingredientRepository.save(newIngredient);
+                                return newIngredient;
+                            });
+
+                    recipeIngredient.setIngredient(ingredient);
+                    recipeIngredient.setQuantity(recipeIngredientDto.getQuantity());
+                    recipeIngredient.setUnit(recipeIngredientDto.getUnit());
+
+                    return recipeIngredient;
+                })
+                .collect(Collectors.toSet())));
+
+        recipeRepository.save(recipe);
+        imageService.uploadImage(image, recipe);
+        recipe.setImage(imageRepository.findByRecipe(recipe).orElseThrow());
+
+        return recipeRepository.save(recipe);
+    }
+}
