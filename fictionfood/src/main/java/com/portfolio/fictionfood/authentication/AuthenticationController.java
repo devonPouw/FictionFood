a19b541368255b9f338e4364854efbbdbd204a9f
Devon Pouw <devon_pouw@hotmail.com>
2024-02-27 12:35:51 +0100

    user can be registered again backend wise

--- fictionfood/src/main/java/com/portfolio/fictionfood/authentication/AuthenticationController.java
+++ fictionfood/src/main/java/com/portfolio/fictionfood/authentication/AuthenticationController.java
@@ -1,15 +1,17 @@
 package com.portfolio.fictionfood.authentication;
 
+import com.portfolio.fictionfood.recipe.Recipe;
+import com.portfolio.fictionfood.recipe.RecipeController;
 import com.portfolio.fictionfood.user.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -21,19 +23,26 @@ public class AuthenticationController {
 
     private final AuthenticationService service;
     private final UserRepository repository;
-
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest request
+    public ResponseEntity<?> register(
+            @RequestPart("register") String registerJson,
+            @RequestPart("avatar") MultipartFile avatar
     ) {
-        if (repository.existsByUsername(request.getUsername())) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } else if (repository.existsByEmail(request.getEmail())) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } else if (repository.existsByNickname(request.getNickname())) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } else {
-            return new ResponseEntity<>(service.register(request), HttpStatus.CREATED);
+//        if (repository.existsByUsername(request.getUsername())) {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        } else if (repository.existsByEmail(request.getEmail())) {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        } else if (repository.existsByNickname(request.getNickname())) {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+        try{
+            AuthenticationResponse registerRequest = service.register(registerJson, avatar);
+            logger.info("User successfully registered with username: {}", registerJson.split(":")[1].split(",")[0]);
+            return ResponseEntity.status(HttpStatus.CREATED).body(registerRequest);
+        } catch (IOException e) {
+            logger.error("Error registering user", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
--- fictionfood/src/main/java/com/portfolio/fictionfood/authentication/AuthenticationService.java
+++ fictionfood/src/main/java/com/portfolio/fictionfood/authentication/AuthenticationService.java
@@ -5,6 +5,9 @@ import com.portfolio.fictionfood.authentication.token.Token;
 import com.portfolio.fictionfood.authentication.token.TokenRepository;
 import com.portfolio.fictionfood.authentication.token.TokenType;
 import com.portfolio.fictionfood.config.JwtService;
+import com.portfolio.fictionfood.image.ImageRepository;
+import com.portfolio.fictionfood.image.ImageService;
+import com.portfolio.fictionfood.recipe.PostRecipeDto;
 import com.portfolio.fictionfood.user.User;
 import com.portfolio.fictionfood.user.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,6 +18,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -25,17 +29,24 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ObjectMapper objectMapper;
     private final JwtService jwtService;
+    private final ImageService imageService;
     private final AuthenticationManager authenticationManager;
+    private final ImageRepository imageRepository;
 
-    public AuthenticationResponse register(RegisterRequest request) {
-        var user = User.builder()
-                .nickname(request.getNickname())
-                .username(request.getUsername())
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(request.getRole())
-                .build();
+    public AuthenticationResponse register(String registerJson, MultipartFile avatar) throws IOException {
+        RegisterRequest request = objectMapper.readValue(registerJson, RegisterRequest.class);
+        var user = new User();
+                user.setNickname(request.getNickname());
+                user.setUsername(request.getUsername());
+                user.setEmail(request.getEmail());
+                user.setPassword(passwordEncoder.encode(request.getPassword()));
+                user.setRole(request.getRole());
+        repository.save(user);
+        imageService.uploadImage(avatar, user);
+        user.setAvatar(imageRepository.findByUser(user).orElseThrow());
+
         var savedUser = repository.save(user);
         var jwtToken = jwtService.generateToken(user);
         var refreshToken = jwtService.generateRefreshToken(user);
