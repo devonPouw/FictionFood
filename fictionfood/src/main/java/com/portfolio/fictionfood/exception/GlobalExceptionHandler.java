b7eb113b496db1302a406bf0cce774e198d81438
Devon Pouw <devon_pouw@hotmail.com>
2024-04-18 12:45:47 +0200

    custom forbidden exception added

--- fictionfood/src/main/java/com/portfolio/fictionfood/exception/GlobalExceptionHandler.java
+++ fictionfood/src/main/java/com/portfolio/fictionfood/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.sql.SQLException;
 
@@ -15,6 +16,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = UnauthorizedException.class)
     public ResponseEntity<?> handleCustomUnauthorized(UnauthorizedException ex) {
         return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(value = ForbiddenException.class)
+    public ResponseEntity<?> handleCustomUnauthorized(ForbiddenException ex) {
+        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(value = {ExpiredJwtException.class})
