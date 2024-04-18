f3156831d3dd979e987830868cd2d78f41795ad8
Devon Pouw <devon_pouw@hotmail.com>
2024-04-18 12:53:21 +0200

    profile error handling added

--- vite-frontend/src/pages/Profile.tsx
+++ vite-frontend/src/pages/Profile.tsx
@@ -1,19 +1,29 @@
 import Footer from "@/components/footer/Footer";
 import NavBar from "@/components/header/NavBar";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useToast } from "@/components/ui/use-toast";
 import { backendApi } from "@/services/ApiMappings";
 import { IProfileData } from "@/types/User";
+import { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 
 const Profile = () => {
   const [profile, setProfile] = useState<IProfileData>();
-
+  const { toast } = useToast();
   const fetchUserProfile = async () => {
     try {
       const response = await backendApi.getProfile();
       setProfile(response.data);
     } catch (error) {
-      console.log(error);
+      if (
+        error instanceof AxiosError &&
+        error.response &&
+        error.response.data
+      ) {
+        toast({
+          description: error.response.data.message,
+        });
+      }
     }
   };
   useEffect(() => {
@@ -27,7 +37,7 @@ const Profile = () => {
       ) : (
         <div className="container">
           <img
-            className="w-1/2 h-fit object-contain rounded-lg"
+            className="w-1/2 h-full object-contain rounded-lg aspect-square"
             src={
               import.meta.env.VITE_HTTPS_BACKEND + `/images/${profile.avatarId}`
             }
